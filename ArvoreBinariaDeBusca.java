7d74174051769f51ffa6f26167547f3d1534e54d
NATALY JESSICA RIBEIRO DE SOUZA SOUZA <u21446@lapa02-2.local>
2025-09-02 17:42:39 -0300

    aula 02/09

--- ArvoreBinariaDeBusca.java
+++ ArvoreBinariaDeBusca.java
@@ -1,112 +1,97 @@
-public class ArvoreBinariaDeBusca <X extends Comparable<X>>
-{
-    private class No
-    {
+public class ArvoreBinariaDeBusca<X extends Comparable<X>> {
+    private class No {
         private No esq;
-        private X  info;
+        private X info;
         private No dir;
 
-        public No (No e, X i, No d)
-        {
-            this.esq  = e;
+        public No(No e, X i, No d) {
+            this.esq = e;
             this.info = i;
-            this.dir  = d;
+            this.dir = d;
         }
 
-        public No (X i)
-        {
-            this.esq  = null;
+        public No(X i) {
+            this.esq = null;
             this.info = i;
-            this.dir  = null;
+            this.dir = null;
         }
 
-        public No getEsq ()
-        {
+        public No getEsq() {
             return this.esq;
         }
 
-        public X getInfo ()
-        {
+        public X getInfo() {
             return this.info;
         }
 
-        public No getDir ()
-        {
+        public No getDir() {
             return this.dir;
         }
 
-        public void setEsq (No e)
-        {
+        public void setEsq(No e) {
             this.esq = e;
         }
-        
-        public void setInfo (X i)
-        {
+
+        public void setInfo(X i) {
             this.info = i;
         }
 
-        public void setDir (No d)
-        {
+        public void setDir(No d) {
             this.dir = d;
         }
-    } //fim da classe No
+    } // fim da classe No
 
     private No raiz;
-    
-    public void guardeUmItem (X i) throws Exception
-    {
-        if (i==null) throw new Exception ("Informacao ausente");
-        
-        if (this.raiz==null)
-        {
-            this.raiz = new No (i);
+
+    public void guardeUmItem(X i) throws Exception {
+        if (i == null)
+            throw new Exception("Informacao ausente");
+
+        if (this.raiz == null) {
+            this.raiz = new No(i);
             return;
         }
-        
-        No atual=this.raiz;
-        for(;;) // forever
+
+        No atual = this.raiz;
+        for (;;) // forever
         {
-            int comparacao=i.compareTo(atual.getInfo());
-            
-            if (comparacao==0) throw new Exception ("Elemento repetido");
-            
-            if (comparacao<0)
-            {
-                if (atual.getEsq()==null)
-                {
-                    atual.setEsq (new No (i));
+            int comparacao = i.compareTo(atual.getInfo());
+
+            if (comparacao == 0)
+                throw new Exception("Elemento repetido");
+
+            if (comparacao < 0) {
+                if (atual.getEsq() == null) {
+                    atual.setEsq(new No(i));
                     return;
-                }
-                else
-                    atual=atual.getEsq();
-            }
-            else // comparacao>0
+                } else
+                    atual = atual.getEsq();
+            } else // comparacao>0
             {
-                if (atual.getDir()==null)
-                {
-                    atual.setDir (new No (i));
+                if (atual.getDir() == null) {
+                    atual.setDir(new No(i));
                     return;
-                }
-                else
-                    atual=atual.getDir();
+                } else
+                    atual = atual.getDir();
             }
         }
     }
-    
-    public boolean temOItem (X i) throws Exception
-    {
+
+    // metodo não recursivo
+    public boolean temOItem(X i) throws Exception {
         if (i == null)
             throw new Exception("Informacao ausente");
-    
+
         No atual = this.raiz;
-    
-        for (;;)
-        {
-            if (atual == null)return false;
-    
+
+        for (;;) {
+            if (atual == null)
+                return false;
+
             int comparacao = i.compareTo(atual.getInfo());
-    
-            if (comparacao == 0)return true;
+
+            if (comparacao == 0)
+                return true;
 
             else if (comparacao < 0)
                 atual = atual.getEsq();
@@ -115,20 +100,24 @@ public class ArvoreBinariaDeBusca <X extends Comparable<X>>
         }
     }
 
+    // metodo recursivo
     public boolean temOItemRecursivo(X i) throws Exception {
 
-        if (i == null)throw new Exception("Informacao ausente");
-    
+        if (i == null)
+            throw new Exception("Informacao ausente");
+
         return this.temOItemRecursao(this.raiz, i);
     }
-    
+
     private boolean temOItemRecursao(No atual, X i) {
 
-        if (atual == null)return false;
-    
+        if (atual == null)
+            return false;
+
         int comparacao = i.compareTo(atual.getInfo());
-    
-        if (comparacao == 0)return true;
+
+        if (comparacao == 0)
+            return true;
 
         else if (comparacao < 0)
             return temOItemRecursao(atual.getEsq(), i);
@@ -136,5 +125,69 @@ public class ArvoreBinariaDeBusca <X extends Comparable<X>>
         else
             return temOItemRecursao(atual.getDir(), i);
     }
+
+    private int getAltura(No r) {
+        if (r == null)
+            return 0;
+
+        int alturaDaSubArvoreEsquerda = this.getAltura(r.getEsq()),
+                alturaDaSubArvoreDireita = this.getAltura(r.getDir());
+
+        // o ? no codigo significa "então" e o : "senão"
+        return alturaDaSubArvoreEsquerda > alturaDaSubArvoreDireita
+                ? alturaDaSubArvoreEsquerda + 1
+                : alturaDaSubArvoreDireita + 1;
+    }
+
+    public int getAltura() {
+        return this.getAltura(this.raiz);
+    }
+
+    // metodo remover
+    // verificar se o item removido foi passado
+    // verificar se a raiz não é nula
+    // verificar se o item existe
+    public void removeItem(X i) throws Exception {
+        System.out.println("iniciou");
+        if (i == null)
+            throw new Exception("informação ausente");
+        if (this.raiz == null)
+            throw new Exception("Arvore vazia ");
+        if (!temOItem(i))
+            throw new Exception("Informação a ser removida não existe");
+
+        No atual = this.raiz;
+        for (;;) {
+
+            int comparacao = i.compareTo(atual.getInfo());
+            No pai = null;
+            No fEsq = null;
+            No fDir = null;
+
+            if (comparacao == 0) {
+                if (atual.getDir() == null && atual.getEsq() == null) {
+                    this.raiz = null;
+                    System.out.println("excluiu");
+                    return;
+                } else
+                    System.out.println("raiz tem filhos ");
+                fEsq = atual.getEsq();
+                fDir = atual.getDir();
+                if (fEsq.getInfo() != null && fDir.getInfo() != null) {
+                    this.raiz = fDir;
+                    return; 
+                }
+
+            }
+
+            return;
+
+        }
+    }
+
+    //exercicios isEspelho() faça um metodo para retornar true ou false caso uma arvore seja o espelho da outra
+    //fazer metodos obrigatorios da arvore
+
     
+
 } // fim da classe ArvoreBinariaDeBusca
--- Main.java
+++ Main.java
@@ -5,45 +5,17 @@ public class Main {
 			new ArvoreBinariaDeBusca<Integer> ();
 
             a.guardeUmItem(10);
-            a.guardeUmItem(6);
             a.guardeUmItem(15);
-            a.guardeUmItem(3);
-            a.guardeUmItem(8);
-            a.guardeUmItem(12);
-            a.guardeUmItem(17);
-
-            System.out.println(a.temOItem(10));
-            System.out.println(a.temOItem(6));
-            System.out.println(a.temOItem(15));
-            System.out.println(a.temOItem(3));
-            System.out.println(a.temOItem(8));
-            System.out.println(a.temOItem(12));
-            System.out.println(a.temOItem(17));
-
-            System.out.println(a.temOItem(2));
-            System.out.println(a.temOItem(4));
-            System.out.println(a.temOItem(7));
-            System.out.println(a.temOItem(9));
-            System.out.println(a.temOItem(11));
-            System.out.println(a.temOItem(13));
-            System.out.println(a.temOItem(16));
-            System.out.println(a.temOItem(18));
-
-            System.out.println(a.temOItemRecursivo(10));
-            System.out.println(a.temOItemRecursivo(6));
-            System.out.println(a.temOItemRecursivo(15));
-            System.out.println(a.temOItemRecursivo(3));
-            System.out.println(a.temOItemRecursivo(8));
-            System.out.println(a.temOItemRecursivo(12));
-            System.out.println(a.temOItemRecursivo(17));
-
-            System.out.println(a.temOItemRecursivo(2));
-            System.out.println(a.temOItemRecursivo(4));
-            System.out.println(a.temOItemRecursivo(7));
-            System.out.println(a.temOItemRecursivo(9));
-            System.out.println(a.temOItemRecursivo(11));
-            System.out.println(a.temOItemRecursivo(13));
-            System.out.println(a.temOItemRecursivo(18));
+            a.guardeUmItem(20);
+            //a.guardeUmItem(3);
+            //a.guardeUmItem(6);
+            //a.guardeUmItem(9);
+            int tamanho = a.getAltura();
+            System.out.println(tamanho);
+            
+            
+           //a.removeItem(10);
+           //a.removeItem(11); 
         }
 
         catch (Exception erro){
@@ -52,3 +24,20 @@ public class Main {
     }
     
 }
+
+/*
+            System.out.println(a.temOItem(10));
+            System.out.println(a.temOItem(6));
+            System.out.println(a.temOItem(15));
+            System.out.println(a.temOItem(3));
+            System.out.println(a.temOItem(8));
+            System.out.println(a.temOItem(12));
+            System.out.println(a.temOItem(17));
+            System.out.println(a.temOItemRecursivo(2));
+            System.out.println(a.temOItemRecursivo(4));
+            System.out.println(a.temOItemRecursivo(7));
+            System.out.println(a.temOItemRecursivo(9));
+            System.out.println(a.temOItemRecursivo(11));
+            System.out.println(a.temOItemRecursivo(13));
+            System.out.println(a.temOItemRecursivo(18));
+ */
